53e59d966ed38c23651526de9b6f39021ec318d4
CHAVECO BOLADÃO <kleber.ferreira.lima@escola.pr.gov.br>
2026-05-12 08:30:38 -0300

    Add files via upload

---
+++ Aula01.js
@@ -0,0 +1,9 @@
+//alert("O carpa capim fica batendo no amiguinho!!!")
+
+console.log("Primeiro código em JS");
+console.log(4+5);
+console.log( 3 * 4 );
+console.log( true );
+console.log( 3.89 );
+
+console.log( "Kleber"+ 23 + 1.83, true );
---
+++ Aula02.js
@@ -0,0 +1,10 @@
+//DEFINIÇÃO DE VARIÁVEIS
+
+let pessoa = "Nome da pessoa";
+let idadePessoa = 30;
+let alturaPessoa = 1.93;
+let statusPessoa = true;
+let salarioPessoa = 1500;
+let salarioBonus = 100;
+
+let salarioTotal = salarioBonus + salarioPessoa; 

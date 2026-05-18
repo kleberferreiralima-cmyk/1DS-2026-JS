d0c72005d6ed6cc1fb9ee08ba24d6e8f0d6695da
CHAVECO BOLADÃO <kleber.ferreira.lima@escola.pr.gov.br>
2026-05-18 09:55:43 -0300

    Add files via upload

---
+++ Aula03.js
@@ -0,0 +1,20 @@
+let nome = "Kleber"
+
+if (nome == "Kleber"){
+console.log("Voce digitou o nome correto!")
+}else{
+    console.log("Voce digitou outra coisa...")
+}
+
+let idade = 18;
+
+if (idade <= 18){
+ console.log("Voce é praticamente adulto...")
+}else if( idade < 18){
+    console.log("Voce ainda é de menor...")
+}else{
+    console.log("Voce é adulto...")
+}
+
+//FAÇA UMA VERIFICAÇÃO COM IF E ELSE
+//DE UMA PESSOA SE ELA PODE OU NÃO VOTAR.
